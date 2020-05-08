0b995bb41505d7da49a1166b880aebf70898e825
Vijay <vijaysn02@gmail.com>
2020-05-08 11:59:10 +0530

    Initial Commit

---
+++ AR plane/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AR plane
+//
+//  Created by TPFLAP146 on 08/05/20.
+//  Copyright © 2020 vijay. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
